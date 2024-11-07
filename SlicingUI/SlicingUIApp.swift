8622af0d5c340a2fc8acb691d5fbdf84ab6470c4
Arzu95 <muhammadarzu3432@gmail.com>
2024-11-07 17:21:30 +0700

    Initial Commit

---
+++ SlicingUI/SlicingUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlicingUIApp.swift
+//  SlicingUI
+//
+//  Created by Muhammad Arzu on 07/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SlicingUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
